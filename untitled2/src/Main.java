30a67c6f943aacaba29ecb41afb2c6040fc59c9d
Ivan Neshin <ivan.neshin@simbirsoft.com>
2022-09-14 14:31:02 +0400

    Изменения

--- untitled2/src/Main.java
+++ untitled2/src/Main.java
@@ -6,17 +6,19 @@ import java.util.Scanner;
 import java.util.stream.Stream;
 
 public class Main {
-    static String[] mas = {"Обычный", "профи", "юселесс"};
-    public static void main(String[] args){
-    Scanner scanner = new Scanner(System.in);
-    ArrayList<UsualPeople> arrayList = new ArrayList<>();
-    int n = scanner.nextInt();
-    for(int i = 0; i < n;i++){
-        arrayList.add(new UsualPeople(String.format("Я %s человек", mas[i%3])));
 
-    }
-    for (UsualPeople u : arrayList){
-        u.talk();
-    }
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<UsualPeople> arrayList = new ArrayList<>();
+        int n = scanner.nextInt();
+        for(int i = 0; i < n;i++){
+            if (i%3 == 0) arrayList.add(new UsualPeople()));
+            if (i%3 == 1) arrayList.add(new Professionals()));
+            if (i%3 == 2) arrayList.add(new ForeignProfesionals()));
+        }
+        for (UsualPeople u : arrayList){
+            u.talk();
+        }
     }
 }
