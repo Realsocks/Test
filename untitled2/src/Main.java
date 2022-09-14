9e2bf25f6e1d28fedb757554c70b8bd3935d4546
Ivan Neshin <ivan.neshin@simbirsoft.com>
2022-09-14 16:42:39 +0400

    val =

--- untitled2/src/Main.java
+++ untitled2/src/Main.java
@@ -10,12 +10,14 @@ public class Main {
     public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
+        People val;
         ArrayList<UsualPeople> arrayList = new ArrayList<>();
         int n = scanner.nextInt();
         for(int i = 0; i < n;i++){
-            if (i%3 == 0) arrayList.add(new UsualPeople()));
-            if (i%3 == 1) arrayList.add(new Professionals()));
-            if (i%3 == 2) arrayList.add(new ForeignProfesionals()));
+            if (i%3 == 0) val = new UsualPeople()));
+            if (i%3 == 1) val = new Professionals()));
+            if (i%3 == 2) val = new ForeignProfesionals()));
+            arrayList.add(val);
         }
         for (UsualPeople u : arrayList){
             u.talk();
